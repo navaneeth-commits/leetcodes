d2b9636691d08c14864a30b66879f9defa13a9e4
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-07-30 15:49:45 +0530

    Time: 2 ms (99.62%), Space: 44.6 MB (60.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //int n=s.length();
+        if (s.length() != t.length()) return false;
         int[] freq=new int[26];
         for(char c:s.toCharArray()){
             freq[c-'a']++;
