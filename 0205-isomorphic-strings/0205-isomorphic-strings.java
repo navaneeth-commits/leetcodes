1c24c17cce78c1be898d4a18097dfd431b0a1c6f
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-03 10:48:35 +0530

    Time: 16 ms (47.69%), Space: 44 MB (26.87%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,31 +1,34 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length()==31000){
-            return !(t.charAt(t.length()-3)=='@');
-        }
-        
-        if(s.length()!= t.length()){
+
+        if (s.length() != t.length())
             return false;
+
+        HashMap<Character, Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            // Check s -> t
+            if (mapST.containsKey(a)) {
+                if (mapST.get(a) != b)
+                    return false;
+            } else {
+                mapST.put(a, b);
+            }
+
+            // Check t -> s
+            if (mapTS.containsKey(b)) {
+                if (mapTS.get(b) != a)
+                    return false;
+            } else {
+                mapTS.put(b, a);
+            }
         }
 
-        HashMap<Character,Integer> charIndexS=new HashMap<>();
-        HashMap<Character,Integer> charIndexT=new HashMap<>();
-
-        for(int i=0; i<s.length();i++){
-            char ss=s.charAt(i);
-            char tt=t.charAt(i);
-            if(!charIndexS.containsKey(ss)){
-                charIndexS.put(ss,i);
-            }
-            if(!charIndexT.containsKey(tt)){
-                charIndexT.put(tt,i);
-            }
-
-            if(!charIndexS.get(ss).equals(charIndexT.get(tt))){
-                return false;
-            }
-        }
         return true;
-        
     }
 }
