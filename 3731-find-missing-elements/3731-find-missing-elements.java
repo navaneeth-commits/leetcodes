1689d85b04fc053d13c1f4605711d37444ef2164
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-04 10:01:35 +0530

    Time: 3 ms (81.76%), Space: 46.8 MB (53.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,20 +1,16 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
+        Set<Integer> set=new HashSet<>();
         int max=Integer.MIN_VALUE;
         int min=Integer.MAX_VALUE;
+        for(int i:nums){
+            set.add(i);
+            max=Math.max(max,i);
+            min=Math.min(min,i);
+        }
         List<Integer> list=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            max=Math.max(max,nums[i]);
-            min=Math.min(min,nums[i]);
-        }
-        int[] freq=new int[max-min+1];
-        for(int i=0;i<nums.length;i++){
-            freq[nums[i]-min]++;
-        }
-        for(int i=0;i<freq.length;i++){
-            if(freq[i]==0){
-                list.add(i+min);
-            }
+        for(int i=min+1;i<max;i++){
+            if(!set.contains(i))list.add(i);
         }
         return list;
     }
