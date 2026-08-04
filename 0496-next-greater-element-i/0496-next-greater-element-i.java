e64c2f07aa79670eb59de2ad88896fc2848db564
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-04 10:13:18 +0530

    Time: 2 ms (99.49%), Space: 45.7 MB (11.13%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            int index = -1;
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == nums1[i]) {
-                    index = j;
-                    break;
-                }
+        int[] nextGreater = new int[10001];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
             }
-            ans[i] = -1;
-            for (int j = index + 1; j < nums2.length; j++) {
-                if (nums2[j] > nums1[i]) {
-                    ans[i] = nums2[j];
-                    break;
-                }
-            }
+            nextGreater[nums2[i]] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums2[i]);
         }
-        return ans;
+
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = nextGreater[nums1[i]];
+        }
+
+        return nums1;
     }
 }
