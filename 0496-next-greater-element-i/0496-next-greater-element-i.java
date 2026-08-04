7eff459d14d4f5fe74ad37e7699e7b44ef9d6e0e
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-04 10:16:08 +0530

    Time: 4 ms (73.91%), Space: 45.5 MB (11.13%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,25 +1,31 @@
 class Solution {
     static {
-        for(int i=0;i<500;i++){
+        for (int i = 0; i < 500; i++) {
             new Solution();
         }
     }
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] nextGreater = new int[10001];
-        Stack<Integer> stack = new Stack<>();
 
-        for (int i = nums2.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
-                stack.pop();
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer, Integer> nextGreater = new HashMap<>();
+
+        for (int num : nums2) {
+
+            while (!st.isEmpty() && st.peek() < num) {
+                nextGreater.put(st.pop(), num);
             }
-            nextGreater[nums2[i]] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(nums2[i]);
+
+            st.push(num);
         }
+
+        while (!st.isEmpty()) {
+            nextGreater.put(st.pop(), -1);
+        }
+        int[] ans = new int[nums1.length];
 
         for (int i = 0; i < nums1.length; i++) {
-            nums1[i] = nextGreater[nums1[i]];
+            ans[i] = nextGreater.get(nums1[i]);
         }
-
-        return nums1;
+        return ans;
     }
 }
