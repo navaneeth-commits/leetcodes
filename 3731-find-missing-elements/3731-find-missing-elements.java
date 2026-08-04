e46b791c5c507293ca3588ef31ef5a3aa273cb97
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-04 10:05:28 +0530

    Time: 1 ms (100%), Space: 46.9 MB (42.16%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,21 +1,20 @@
 class Solution {
-    static{
-        for(int i=0;i<500;i++){
-            new Solution();
-        }
-    }
     public List<Integer> findMissingElements(int[] nums) {
-        Set<Integer> set=new HashSet<>();
         int max=Integer.MIN_VALUE;
         int min=Integer.MAX_VALUE;
-        for(int i:nums){
-            set.add(i);
-            max=Math.max(max,i);
-            min=Math.min(min,i);
-        }
         List<Integer> list=new ArrayList<>();
-        for(int i=min+1;i<max;i++){
-            if(!set.contains(i))list.add(i);
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(max,nums[i]);
+            min=Math.min(min,nums[i]);
+        }
+        int[] freq=new int[max-min+1];
+        for(int i=0;i<nums.length;i++){
+            freq[nums[i]-min]++;
+        }
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]==0){
+                list.add(i+min);
+            }
         }
         return list;
     }
