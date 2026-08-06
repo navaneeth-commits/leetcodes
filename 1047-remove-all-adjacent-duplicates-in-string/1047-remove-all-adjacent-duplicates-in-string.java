97d3424822366a425b9782a8881df92e0c83b325
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-06 18:38:58 +0530

    Time: 16 ms (73.31%), Space: 46.5 MB (92.44%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuilder stack =new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(stack.length()==0) stack.append(s.charAt(i));
+            else if(stack.length()!=0){
+                if(stack.charAt(stack.length()-1)==s.charAt(i)) stack.deleteCharAt(stack.length()-1);
+                else stack.append(s.charAt(i));
+            }
+        }
+        return stack.toString();
+    }
+}
