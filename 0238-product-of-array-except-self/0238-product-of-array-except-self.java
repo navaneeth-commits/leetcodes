8deacd68a780e5cc8b229d43149d7a9abf2ba9e7
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-06 20:12:20 +0530

    Time: 1 ms (99.85%), Space: 71.8 MB (59.12%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,20 +1,21 @@
 class Solution {
     static {
-        for(int i=0;i<500;i++){
-            new Solution();
+        for (int i = 0; i < 500; i++) {
+            productExceptSelf(new int[] { 0, 1 });
         }
     }
-    public int[] productExceptSelf(int[] nums) {
-        int n=nums.length;
-        int[] res=new int[n];
-        res[0]=1;
-        for(int i=1;i<n;i++){
-            res[i]=res[i-1]*nums[i-1];
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
         }
-        int suff=1;
-        for(int i=n-1;i>=0;i--){
-            res[i]=res[i]*suff;
-            suff*=nums[i];
+        int suff = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] = res[i] * suff;
+            suff *= nums[i];
         }
         return res;
     }
