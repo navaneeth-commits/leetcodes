5b6d35f25486a73dfc2c5f4afa22d942d6935a47
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-09 12:24:46 +0530

    Time: 0 ms (100%), Space: 41.8 MB (87.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<=3)return n;
-        int prev1=3;
-        int prev2=2;
-        int cur=0;
-        for(int i=3;i<n;i++){
-            cur=prev1+prev2;
-            prev2=prev1;
-            prev1=cur;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+    }
+    public int solve(int n,int[] dp){
+        if(n<=2) return n;
+        if(dp[n]!=-1){
+            return dp[n];
         }
-        return cur;
+        dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
     }
 }
