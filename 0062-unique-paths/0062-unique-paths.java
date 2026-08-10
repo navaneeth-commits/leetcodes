8c6ab0d0fef222c2340da1cff12b82280d6d7243
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-10 17:14:30 +0530

    Time: 0 ms (100%), Space: 42.4 MB (13.16%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,16 +1,16 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-
-        int numer = m + n - 2;
-        int denom = Math.max(m - 1, n - 1);
-        int other = Math.min(m - 1, n - 1);
-
-        long ans = 1;
-
-        for (int i = 1; i <= other; i++) {
-            ans = ans * (denom + i) / i;
+        int[][] dp=new int[m][n];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
-
-        return (int) ans;
+        return solve(0,0,dp);
+    }
+    public int solve(int i,int j,int[][] dp){
+        if(i>=dp.length || j>=dp[0].length)return 0;
+        if(i == dp.length-1 && j == dp[0].length-1)return 1;
+        if(dp[i][j]!=-1)return dp[i][j];
+        dp[i][j]=solve(i+1,j,dp)+solve(i,j+1,dp);
+        return dp[i][j];
     }
 }
