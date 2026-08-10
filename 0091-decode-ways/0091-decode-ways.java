ba60b7f0fdf17a0ca34ddcb5d8492f7fbf844019
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-10 12:02:55 +0530

    Time: 1 ms (79.44%), Space: 43.6 MB (5.08%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,4 +1,9 @@
 class Solution {
+    static{
+        for(int i=0;i<500;i++){
+            new Solution();
+        }
+    }
     public int numDecodings(String s) {
         if(s.charAt(0)=='0') return 0;
         int[] dp=new int[s.length()];
