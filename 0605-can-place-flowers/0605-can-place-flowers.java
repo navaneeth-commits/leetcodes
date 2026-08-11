81ecbc31be051e8c91644128225bb82f7f0db074
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-11 16:02:57 +0530

    Time: 1 ms (98.77%), Space: 47.7 MB (77%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count=0;
+        if(flowerbed.length==1 && flowerbed[0]==0){
+            count=1;
+            return true;
+        }
+        if(flowerbed[0]==0 && flowerbed[1]==0){
+            flowerbed[0]=1;
+            count++;
+        }
+        for(int i=1;i<flowerbed.length-1;i++){
+            if(flowerbed[i-1]== 0 && flowerbed[i+1]==0 && flowerbed[i]==0){
+                flowerbed[i]=1;
+                count++;
+            }
+        }
+        if(flowerbed[flowerbed.length-1]==0 && flowerbed[flowerbed.length-2]==0){
+            flowerbed[flowerbed.length-1]=1;
+            count++;
+        }
+        return count>=n;
+    }
+}
