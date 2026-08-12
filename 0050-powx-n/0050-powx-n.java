2ead3a0995443ede5c878377e2ba75f3e109e907
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-12 07:41:43 +0530

    Time: 0 ms (100%), Space: 47.7 MB (78.76%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        long N = n;
-
-        if (N < 0)
-            return 1.0 / power(x, -N);
-
-        return power(x, N);
+        long N=n;
+        if(n==0)return 1;
+        return power(x,N);
     }
-
-    private double power(double x, long n) {
-        if (n == 0)
-            return 1;
-
-        if (n % 2 == 0)
-            return power(x * x, n / 2);
-
-        return x * power(x, n - 1);
+    public double power(double x,long N){
+        if(N==0) return 1;
+        if(N==1)return x;
+        if(N>0){
+            if(N%2==0){
+                return power(x*x,N/2);
+            }else{
+                return x*power(x,N-1);
+            }
+        }else{
+            return 1.0/power(x,-N);
+        }
     }
 }
