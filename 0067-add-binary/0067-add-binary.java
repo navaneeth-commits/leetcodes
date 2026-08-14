98ff4524d27e396650542351e11bf5994c3c5e0e
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-14 19:13:47 +0530

    Time: 3 ms (13.71%), Space: 43.3 MB (93.05%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -3,40 +3,39 @@ class Solution {
         int i=a.length()-1;
         int j=b.length()-1;
         char carry='0';
-        String res="";
+        StringBuilder res=new StringBuilder();
         while(i>=0 || j>=0 || carry!='0'){
             char x=((i>=0)?a.charAt(i):'0');
             char y=((j>=0)?b.charAt(j):'0');
             if(carry=='0'){
                 if(x=='1' && y=='1'){
                     carry='1';
-                    res+='0';
+                    res.append('0');
                 }else if(x=='1'||y=='1'){
                     carry='0';
-                    res+='1';
+                    res.append('1');
                 }else{
                     carry='0';
-                    res+='0';
+                    res.append('0');
                 }
             }else{
                 if(x=='1' && y=='1'){
                     carry='1';
-                    res+='1';
+                    res.append('1');
                 }else if(x=='1'||y=='1'){
                     carry='1';
-                    res+='0';
+                    res.append('0');
                 }else{
                     carry='0';
-                    res+='1';
+                    res.append('1');
                 }
             }
             i--;
             j--;
         }
-        res+=carry;
-        StringBuilder result=new StringBuilder(res);
-        result=result.reverse();
-        if(result.charAt(0)=='0')result.deleteCharAt(0);
-        return result.toString();
+        res.append(carry);
+        res=res.reverse();
+        if(res.charAt(0)=='0')res.deleteCharAt(0);
+        return res.toString();
     }
 }
