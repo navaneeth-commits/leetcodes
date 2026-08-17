2549f2ca660b3d5e760da2a3b70b7fcc9827d1fb
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-17 10:27:47 +0530

    Time: 1 ms (99.94%), Space: 46.6 MB (96.44%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,9 @@
 class Solution {
-    static {
-        for (int i = 0; i < 400; i++) {
-            singleNumber(new int[0]);
+    public int singleNumber(int[] nums) {
+        int xor=0;
+        for(int num:nums){
+            xor^=num;
         }
-    }
-    public static int singleNumber(int[] nums) {
-        int ans = 0;
-
-        for (int x : nums) {
-            ans ^= x;
-        }
-
-        return ans;
+        return xor;
     }
 }
