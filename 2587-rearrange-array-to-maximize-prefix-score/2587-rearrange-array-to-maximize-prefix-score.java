e4be6799c05be240f60b084e0f8d6cc0013de49c
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-08-29 10:56:40 +0530

    Time: 22 ms (100%), Space: 77.8 MB (95.72%) - LeetHub

--- 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.java
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.java
@@ -1,12 +1,38 @@
 class Solution {
-    public int maxScore(int[] nums) {
-        Arrays.sort(nums);
-        int count=0;
-        long sum=0;
-        for(int i=nums.length-1;i>=0;i--){
-            sum+=nums[i];
-            if(sum>0) count++;
+    static{
+        for(int i=0;i<500;i++){
+            maxScore(new int[]{0,0});
         }
-        return count;
+    }
+    public static int maxScore(int[] nums) {
+        long sum = 0;
+        int positives = 0, zeroes = 0, negatives = 0;
+        for (int num : nums) {
+            if (num > 0) {
+                sum += num;
+                positives++;
+            } else if (num == 0) {
+                zeroes++;
+            } else {
+                negatives++;
+            }
+        }
+        if (positives <= 0) return 0;
+        int[] negativeArr = new int[negatives];
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                negativeArr[k] = nums[i];
+                k++;
+            }
+        }
+        int score = zeroes + positives;
+        Arrays.sort(negativeArr);
+        for (int i = negativeArr.length - 1; i >= 0; i--) {
+            sum += negativeArr[i];
+            if (sum <= 0) break;
+            score++;
+        }
+        return score;
     }
 }
