50b5aecae204a99ab51c59a87baf0f62e2926d13
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-09-08 10:46:01 +0530

    Time: 1 ms (83.65%), Space: 42.5 MB (72.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,9 +1,4 @@
 class Solution {
-    static{
-        for(int i=0;i<500;i++){
-            countCommas(0);
-        }
-    }
     public static int countCommas(int n) {
         if(n<1000)return 0;
         return n-999;
