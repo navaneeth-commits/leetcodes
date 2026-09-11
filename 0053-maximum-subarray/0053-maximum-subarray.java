074118ec1d8b0fd4cf8ecf09d6006a574b8e4c4e
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-09-11 20:14:03 +0530

    Time: 1 ms (99.92%), Space: 77 MB (82.64%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=nums[0];
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum=Math.max(nums[i],sum+nums[i]);
+            max=Math.max(max,sum);
+        }
+        return max;
+    }
+}
