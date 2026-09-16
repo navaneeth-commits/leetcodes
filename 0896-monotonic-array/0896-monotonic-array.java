adc0de99b7f9cecf25dae00ff662a336f32b8b89
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-09-16 20:05:56 +0530

    Time: 0 ms (100%), Space: 85.2 MB (39.45%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,17 +1,24 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        int n=nums.length;
-        if(n==1)return true;
-        
-        boolean inc=true;
-        boolean dec=true;
 
-        for(int i=1;i<n;i++){
-            if(!inc && !dec)return false;
-            if(nums[i]<nums[i-1])inc=false;
-            if(nums[i]>nums[i-1])dec=false;
-            
-        }
-        return inc||dec;
+        int n = nums.length;
+
+        int i = 1;
+
+        while(i < n && nums[i-1]<= nums[i]) i++;
+
+        if(i == n) return true;
+
+        i = 1;
+
+            while(i<n && nums[i-1] >= nums[i]) i++;
+        
+
+        if(i == n) return  true;
+
+        return false;
+
+
     }
+
 }
