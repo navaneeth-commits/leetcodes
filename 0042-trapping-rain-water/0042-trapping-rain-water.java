ce88ce800f84d4d845d8a8fa7d9a1b2e19df6d2a
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-09-16 19:08:29 +0530

    Time: 0 ms (100%), Space: 47.9 MB (42.71%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,7 +1,9 @@
 class Solution {
     public int trap(int[] height) {
         int n=height.length;
-        int left=0,right=n-1,leftmax=height[0],rightmax=height[n-1],water=0;
+        int left=0,leftmax=height[left];
+        int right=n-1,rightmax=height[right];
+        int water=0;
         while(left<right){
             if(leftmax<rightmax){
                 left++;
