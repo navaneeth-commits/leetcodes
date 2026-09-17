1c5321f5580503520182b2e9a54265096bcfd1a4
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-09-17 10:37:01 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.84%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,18 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(n,dp);
+        return fibo(n);
     }
-    public int solve(int n,int[] dp){
-        if(n<=2) return n;
-        if(dp[n]!=-1){
-            return dp[n];
+    public int fibo(int n){
+        int i=0;
+        int b=1;
+        int a=0;
+        int res=0;
+        while(i<n){
+            res=a+b;
+            a=b;
+            b=res;
+            i++;
         }
-        dp[n]=solve(n-1,dp)+solve(n-2,dp);
-        return dp[n];
+        return b;
     }
 }
