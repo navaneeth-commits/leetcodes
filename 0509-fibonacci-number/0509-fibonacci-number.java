d9181d6327dd3cbb84d28e356165e32c58448506
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-09-17 10:41:47 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,15 @@
 class Solution {
     public int fib(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return solve(n, dp);
-    }
-    public static int solve(int n, int[] dp) {
-        if (n <= 1) {
-            return n;
+        int i=0;
+        int b=1;
+        int a=0;
+        int res=0;
+        while(i<n){
+            res=a+b;
+            a=b;
+            b=res;
+            i++;
         }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-        return dp[n];
+        return a;
     }
 }
