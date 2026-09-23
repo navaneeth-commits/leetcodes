4cce79a60f41439b7b22b2f39c8ca73c9f32f3a3
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-09-23 13:13:34 +0530

    Time: 2 ms (70.63%), Space: 55.1 MB (47.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,11 +5,9 @@
  */
 var twoSum = function(nums, target) {
     const map=new Map();
-    
-    for(let i=0;i<nums.length;i++){
+    for(let i =0;i<nums.length;i++){
         if(map.has(target-nums[i]))return [i,map.get(target-nums[i])];
         map.set(nums[i],i);
     }
-    
-    return [];
+    return [-1,-1];
 };
