96d72768793bbd9e4584425820691ed356fcf64e
navaneeth-commits <kanasaninavaneethreddy@gmail.com>
2026-09-24 09:02:56 +0530

    Time: 1 ms (99.69%), Space: 45.7 MB (24.38%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -1,11 +1,16 @@
 class Solution {
-    public int smallestIndex(int[] nums) {
+    static{
+        for(int i=0;i<500;i++){
+            smallestIndex(new int[]{0,0});
+        }
+    }
+    public static int smallestIndex(int[] nums) {
         for(int i=0;i<nums.length;i++){
             if(i==digitsum(nums[i]))return i;
         }
         return -1;
     }
-    public int digitsum(int n){
+    public static int digitsum(int n){
         int sum=0;
         while(n>0){
             sum+=n%10;
